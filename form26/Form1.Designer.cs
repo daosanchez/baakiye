58bfdafa4a82c0851fbe71da4c2c18845428fb94
sanchez <145916188+ravzasanchez@users.noreply.github.com>
2023-10-10 15:13:56 +0300

    Add files via upload

---
+++ form26/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace form26
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTopla = new System.Windows.Forms.Button();
+            this.lblBakiye = new System.Windows.Forms.Label();
+            this.btnOde = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTopla
+            // 
+            this.btnTopla.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnTopla.Font = new System.Drawing.Font("Algerian", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTopla.Location = new System.Drawing.Point(118, 38);
+            this.btnTopla.Name = "btnTopla";
+            this.btnTopla.Size = new System.Drawing.Size(155, 104);
+            this.btnTopla.TabIndex = 0;
+            this.btnTopla.Text = "Topla";
+            this.btnTopla.UseVisualStyleBackColor = false;
+            this.btnTopla.Click += new System.EventHandler(this.btnTopla_Click);
+            // 
+            // lblBakiye
+            // 
+            this.lblBakiye.AutoSize = true;
+            this.lblBakiye.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblBakiye.Font = new System.Drawing.Font("Algerian", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBakiye.Location = new System.Drawing.Point(33, 301);
+            this.lblBakiye.Name = "lblBakiye";
+            this.lblBakiye.Size = new System.Drawing.Size(117, 30);
+            this.lblBakiye.TabIndex = 1;
+            this.lblBakiye.Text = "Bakiye:";
+            // 
+            // btnOde
+            // 
+            this.btnOde.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnOde.Font = new System.Drawing.Font("Algerian", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOde.Location = new System.Drawing.Point(379, 259);
+            this.btnOde.Name = "btnOde";
+            this.btnOde.Size = new System.Drawing.Size(156, 72);
+            this.btnOde.TabIndex = 2;
+            this.btnOde.Text = "öde:";
+            this.btnOde.UseVisualStyleBackColor = false;
+            this.btnOde.Click += new System.EventHandler(this.btnOde_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(547, 407);
+            this.Controls.Add(this.btnOde);
+            this.Controls.Add(this.lblBakiye);
+            this.Controls.Add(this.btnTopla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTopla;
+        private System.Windows.Forms.Label lblBakiye;
+        private System.Windows.Forms.Button btnOde;
+    }
+}
+
---
+++ form26/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form26
+{
+    public partial class Form1 : Form
+    {
+        //buradaki değişkenler globaldir.
+        double bakiye = 20;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblBakiye.Text = "Bakiye :" +bakiye.ToString();
+            lblBakiye.ForeColor = Color.DarkGreen;
+        }
+
+        private void btnTopla_Click(object sender, EventArgs e)
+        {
+            bakiye += 50;
+            lblBakiye.Text = "Bakiye :" + bakiye.ToString();
+
+        }
+
+        private void btnOde_Click(object sender, EventArgs e)
+        {
+            if (bakiye >= 8)
+            {
+                bakiye -= 8;
+                lblBakiye.Text = "Bakiye :" + bakiye.ToString();
+            }
+            else
+            {
+
+                lblBakiye.Text = "Paran Yok Pis Fakir...";
+                lblBakiye.ForeColor = Color.Red;
+            }
+        }
+
+    }
+}
